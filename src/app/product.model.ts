0eaa6cef93ca7b48e8086341ca51689d3cf8b0c9
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-01 09:05:16 -0800

    adds product model and sample products json file

---
+++ src/app/product.model.ts
@@ -0,0 +1,3 @@
+export class Product {
+  constructor ( public title: string, public category: string, public price: string, public description: string, public colors: string[], public image: string[]) {}
+}
